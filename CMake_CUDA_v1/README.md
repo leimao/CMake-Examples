ffb356fde570eb9b0edfa6f72f9f0923bff740ca
Lei Mao <dukeleimao@gmail.com>
2019-04-13 23:34:21 -0700

    update

---
+++ CMake_CUDA_v1/README.md
@@ -0,0 +1,28 @@
+# CMake CUDA
+
+## Introduction
+
+This project talked about how to use CMake compile CUDA files.
+
+## To Build and Compile
+
+To compile the libraries and executables, please run the following command in the terminal.
+
+```bash
+$ mkdir build 
+$ cd build 
+$ cmake ..
+$ make
+```
+
+To run the executables, please run the following command in the terminal.
+
+```bash
+$ ./CMakeCUDAExample 
+```
+
+The following messages should show up if the program was configured and compiled correctly.
+```bash
+$ ./CMakeCUDAExample 
+Max error: 0
+```
---
+++ CMake_CUDA_v1/main.cu
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <cuda.h>
+
+__global__
+void add(int n, float * x, float * y)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride)
+    {
+        y[i] = x[i] + y[i];
+    }
+}
+
+int main()
+{
+    int N = 1<<20;
+    float *x, *y;
+    
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
+    
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) 
+    {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Run the kernel
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    // add<<<1, blockSize>>>(N, x, y);
+    add<<<numBlocks, blockSize>>>(N, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i]-3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+  
+    return 0;
+}
